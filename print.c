f10474e2ac7754ab541f2596bae7eb6f206a6d9b
Creationdis <Creationdis@gmail.com>
2023-05-10 06:57:31 -0700

    ldkfgùm

---
+++ print.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print - print char.
+ * @new_str: string.
+ *
+ * Return: string length.
+ **/
+int print(char *new_str)
+{
+int i;
+
+for (i = 0; new_str[i] != '\0'; ++i)
+_putchar(new_str[i]);
+
+return (i);
+}
+
