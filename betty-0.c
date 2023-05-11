7905fb17d881290a46ced94a13cef8206009fc8e
Creationdis <Creationdis@gmail.com>
2023-05-11 14:54:36 -0700

    djknsmdlkjf

---
+++ betty-0.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include "shell.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+int n1;
+int n2;
+int result;
+
+printf("please enter a number: ");
+scanf("%d", &n1);
+
+printf("please enter another number: ");
+scanf("%d", &n2);
+
+result = n1 + n2;
+printf("%d\n", result);
+
+return (0);
+}
--- shell.h
+++ shell.h
@@ -5,16 +5,20 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
+#include <sys/types.h>
 #include <sys/wait.h>
 #include <sys/stat.h>
 #include <signal.h>
+#include <limits.h>
 
 #define DELIM " \t\r\n\a"
 #define BUFSIZE 1024
 
 /* Function prototypes */
+
 int _putchar(char c);
-int print(char *new_str)
+int print(char *new_str);
+int main(void);
 int execute_command(char **args);
 int read_command(char *line, char **args);
 void signal_handler(int signum);
