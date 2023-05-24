1e8363ad7213e03eac1e41513d9ba6cd618f27db
Creationdis <Creationdis@gmail.com>
2023-05-24 02:24:59 -0700

    jdfmgl

---
+++ env_builtin.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * env_builtin - function that implements the printenv builtins
+ * @args: the parsed arguments
+ * Return: 0
+ */
+
+extern char **environ;
+
+int env_builtin(char **args)
+{
+char **env = environ;
+while (*env)
+{
+printf("%s\n", *env);
+env++;
+}
+return (0);
+}
+if (strcmp(args[0], "env") == 0) 
+{
+env_builtin(args);
+return (1);
+}
