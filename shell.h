fedca2efe412ceb92650fe7a3aa3850b7e080d8c
Creationdis <Creationdis@gmail.com>
2023-05-26 02:36:51 -0700

    kjg

--- shell.h
+++ shell.h
@@ -1,15 +1,18 @@
 #ifndef SHELL_H
 #define SHELL_H
 
+#include <stddef.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
-#include <string.h>
-#include <sys/types.h>
-#include <sys/wait.h>
 #include <sys/stat.h>
+#include <sys/types.h>
+#include <fcntl.h>
+#include <string.h>
 #include <signal.h>
+#include <sys/wait.h>
 #include <limits.h>
+#include <ctype.h>
 
 #define DELIM " \t\r\n\a"
 #define BUFSIZE 1024
