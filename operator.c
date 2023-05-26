e68790bc6fc2fcc181723e8dad569c5d217ee2eb
fatimanaitraiss <fatimanaitraiss33@gmail.com>
2023-05-26 07:27:22 -0700

    sml-shl

---
+++ operator.c
@@ -0,0 +1,31 @@
+#include "shell.h"
+
+/**
+ * handle_separator_operator - the function handles the separator operators.
+ * @line:  string.
+ * @argv: A vector of arguments.
+ * @env: An array of environment variables.
+ * Return: 1 on success, otherwise 0.
+ */
+int handle_separator_operator(char *line, char **argv, char **env)
+{
+int switch_cmds = 0, i = 0;
+static char *cmds[] = {";", "&&", "||"};
+
+if (line == NULL)
+return (0);
+
+while (i <= 2)
+{
+if (compare_strings(find_substring(line, cmds[i]), cmds[i]) == 0)
+{
+switch_cmds = i + 1;
+break;
+}
+i++;
+}
+
+switch (switch_cmds)
+    
+return (0);
+}
